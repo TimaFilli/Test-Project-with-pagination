e8024b28ab05223b2cf005ede37d4f586ff7ad81
TimaFilli <timafilli15@gmail.com>
2022-06-22 22:16:23 +0500

    middlewares added and works

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+import fileUpload from "express-fileupload"
 import express from 'express'
 import path from 'path'
 import './config.js'
@@ -5,6 +6,9 @@ import UserRouter from './routes/user.js'
 
 import database from './utils/db.js'
 import mockData from './mock.js'
+
+import errorHandlerMiddleware from './middlewares/errorHandler.js'
+import loggerMiddleware from './middlewares/logger.js'
 
 const PORT = process.env.PORT || 5000
 
@@ -14,14 +18,19 @@ const PORT = process.env.PORT || 5000
     const db = await database()
     await mockData({ sequelize: db })
 
-    app.use(express.json())
     app.use(express.static(path.join(process.cwd(), 'src', 'public')))
+    app.use(express.json())
+    app.use(fileUpload())
     app.use((req, res, next) => {
         req.models = db.models
         next()
     })
 
     app.use(UserRouter)
+
+
+    app.use(errorHandlerMiddleware)
+    app.use(loggerMiddleware)
 
     app.listen(PORT, () => console.log('server ready at *' + PORT))
 }()
