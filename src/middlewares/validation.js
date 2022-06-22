a3206342b129e64490a11dce43502b74f6d69680
TimaFilli <timafilli15@gmail.com>
2022-06-22 21:07:22 +0500

    Middlewares added

---
+++ src/middlewares/validation.js
@@ -0,0 +1,41 @@
+import {
+  USER_LOGIN_VALIDATION,
+  USER_REGISTER_VALIDATION,
+} from "../utils/validation.js";
+import { ValidationError } from "../utils/errors.js";
+
+export default (req, res, next) => {
+  try {
+    if (req.url === "/login" && req.method === "POST") {
+      const { error } = USER_LOGIN_VALIDATION.validate({ body: req.body });
+      if (error) {
+        throw new ValidationError(error.message);
+      }
+    }
+
+    if (req.url === "/register" && req.method === "POST") {
+      const { error } = USER_REGISTER_VALIDATION.validate({ body: req.body });
+      if (error) {
+        throw new ValidationError(error.message);
+      }
+    }
+
+    if (req.url === "/users" && req.method === "PUT") {
+      const { error } = PUT_USER_VALIDATION.validate({ body: req.body });
+      if (error) {
+        throw new ValidationError(error.message);
+      }
+    }
+
+    if (req.url === "/users" && req.method === "DELETE") {
+      const { error } = DELETE_USER_VALIDATION.validate({ body: req.body });
+      if (error) {
+        throw new ValidationError(error.message);
+      }
+    }
+
+    return next();
+  } catch (error) {
+    next(error);
+  }
+};
