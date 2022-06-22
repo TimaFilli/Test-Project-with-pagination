43808c013ca51ae3f031ae9b83a90a762286de2d
TimaFilli <timafilli15@gmail.com>
2022-06-22 19:45:51 +0500

    all configurations doned to write user routes

---
+++ src/models/user.js
@@ -0,0 +1,61 @@
+import { DataTypes } from 'sequelize'
+
+export default ({ sequelize }) => {
+
+    sequelize.define('User', {
+        userId: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isAlpha: true
+            }
+        },
+
+        gender: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isIn: {
+                    args: [['male', 'female']],
+                    msg: 'The gender must be male or female!'
+                },
+            }
+        },
+        
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        
+        userImg: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+
+        age: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                min: {
+                    args: 1,
+                    msg: 'The age is too small!'
+                },
+                max: {
+                    args: 100,
+                    msg: 'The age is too large!'
+                }
+            }
+        }
+    }, {
+        underscored: true,
+        tableName: 'users'
+    })
+
+}
